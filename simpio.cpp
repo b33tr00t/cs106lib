4ead7b87a9d2d8e42df500f158edb265da58ce17
b33tr00t <b33tr00t@ymail.com>
2011-06-18 10:13:22 +0200

    modified simpio.cpp

--- simpio.cpp
+++ simpio.cpp
@@ -11,16 +11,30 @@ using namespace std;
  * it as an integer.  The integer value is returned.  If an
  * integer cannot be scanned or if more characters follow the
  * number, the user is given a chance to retry.
- * b33tr00t: as it is now it doesn't checks wether the user input is really an integer and it doesn't give another chance to retry
  */
  
 int GetInteger()
 {
-int n;
-cout<<"(we are looking for an int here): ";
-cin>>n;
-return n;
+  while(true)
+   {
+     stringstream ss;
+     ss << GetLine();
+
+     int returnResult;
+     if(ss >> returnResult)
+      {
+        char remaining;
+        if(ss >> remaining)
+        cout << "Unexpected character: " << remaining << endl<< "Retry: ";
+        else
+        return returnResult;
+      }
+     else
+      cout << "Please enter an integer." << endl<< "Retry: ";      
+   }
 }
+
+
 
 /*
  * Function: GetLong
@@ -30,15 +44,27 @@ return n;
  * it into a long integer.  The long is returned.  If the 
  * number cannot be scanned or if extra characters follow it,
  * the user is given a chance to retry.
- * b33tr00t: no checking and no second chance yet
  */
  
 long GetLong()
 {
-long n;
-cout<<"(we are looking for a long here): ";
-cin>>n;
-return n;
+  while(true)
+   {
+     stringstream ss;
+     ss << GetLine();
+
+     long returnResult;
+     if(ss >> returnResult)
+      {
+        char remaining;
+        if(ss >> remaining)
+        cout << "Unexpected character: " << remaining << endl<< "Retry: ";
+        else
+        return returnResult;
+      }
+     else
+      cout << "Please enter a long." << endl<< "Retry: ";      
+   }
 }
 
 /*
@@ -49,15 +75,27 @@ return n;
  * it as a double.  If the number cannot be scanned or if extra
  * characters follow after the number ends, the user is given
  * a chance to reenter the value.
- * b33tr00t: no checking and no second chance yet
  */
 
 double GetReal()
 {
-double n;
-cout<<"(we are looking for a double here): ";
-cin>>n;
-return n;
+  while(true)
+   {
+     stringstream ss;
+     ss << GetLine();
+
+     double returnResult;
+     if(ss >> returnResult)
+      {
+        char remaining;
+        if(ss >> remaining)
+        cout << "Unexpected character: " << remaining << endl<< "Retry: ";
+        else
+        return returnResult;
+      }
+     else
+      cout << "Please enter a double." << endl<< "Retry: ";      
+   }
 }
 
 /*
@@ -66,16 +104,24 @@ return n;
  * ---------------------
  * GetLine reads a line of text from standard input and returns
  * the line as a string.  The newline character that terminates
- * the input is not stored as part of the string that is returned.
- * b33tr00t: at this stage I'm not sure about this 'neline character'.might need to revise this later
+ * the input is not stored as part of the string that is returnd
  */
  
+
+
 string GetLine()
 {
-string returnString;
-cout<<"(we are looking for a string here): ";
-cin>>returnString;
+  string returnString;
+  while (true)
+  {
+    getline(cin,returnString);
+    if (!cin.fail()) break;
+    else
+     {
+       returnString="";
+       cin.clear();
+     }
+  }	
 return returnString;
 }
-
 
